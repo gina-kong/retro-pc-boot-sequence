d93e83a791632396c92cc3fd4081cae5e4fb9159
Gina Kong <ginagkong@gmail.com>
2025-02-18 00:26:56 +1100

    [feat]: add bios memory and device animation

--- lib/components/Loader.jsx
+++ lib/components/Loader.jsx
@@ -1,63 +1,117 @@
 import styles from './loader.module.css'
 import trophyLogo from '../../src/assets/trophy.png'
 import luminosityLogo from '../../src/assets/luminosity.png'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const Loader = () => {
   let date = new Date();
   date = date.toLocaleDateString();
+
+  const DEVICES = [
+    "ABCDE",
+    "12335",
+    "Potato Tornado",
+    "Howdy"
+  ]
+
+  const [devices, setDevices] = useState(["", "", "", ""])
+  const [memory, setMemory] = useState("0")
+
+  useEffect(() => {
+    const bootSequence = async () => {
+      await checkMemory();
+      await checkDevices();
+    }
+
+    bootSequence();
+  }, []);
+
+  const checkMemory = async () => {
+    return new Promise((resolve) => {
+      const BLOCK = 18;
+      const MAX = 32768;
+      let mem = 0;
+
+      for (let i = 0; i < MAX; i = i + BLOCK) {
+        setTimeout(() => {
+          mem = i;
+          setMemory(`${mem}K`)
+        }, i / BLOCK)
+      }
+
+      setTimeout(() => {
+        setMemory(`${mem}K OK`)
+        resolve();
+      }, MAX / BLOCK - 1200)
+    })
+  }
+
+  const checkDevices = async () => {
+    const detectedDevices = [...devices]
+
+    const detectDevice = async (index, label, delay = 2000) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          detectedDevices[index] = `Detecting IDE ${label}... ${DEVICES[index]}`;
+          setDevices([...detectedDevices])
+          resolve();
+        }, delay);
+      })
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1500))
+    detectedDevices[0] = `Detecting IDE Primary Master... ${DEVICES[0]}`
+    setDevices([...detectedDevices])
+    console.log(devices)
+
+    await detectDevice(1, "Primary Slave")
+    await detectDevice(2, "Secondary Master")
+    await detectDevice(3, "Secondary Slave")
+
+    console.log(devices)
+  }
+
   return (
     <>
-      <div className={styles.loaderContainer}>
-        <div>
-          <div className={styles.paragraph}>
-            <div style={{"display": "flex", "flexDirection": "row"}}>
+      <div className={styles.screen}>
+        <div className={styles.header}>
+          <img src={luminosityLogo} className={styles.energyLogo} alt="energy-star-logo"/>
+
+            <div className={styles.brandText}>
               <img src={trophyLogo} className={styles.trophyLogo} alt="trophy-logo"/>
-              <div>
-                <h3>Trophy Modular BIOS v4.51PG, A Luminosity Star Ally</h3>
-                <h3>Copyright (C) 1997-2025, Trophy Software, Inc.</h3>
-              </div>
+                <p className={styles.paragraphBreak}>
+                  Trophy Modular BIOS v4.51PG, A Luminosity Star Ally
+                  <br />
+                  Copyright (C) 1997-2025, Trophy Software, Inc.
+                </p>
             </div>
+              
+          <p className={styles.paragraphBreak}>(55XWUQOE) Recon r430VX PCIset(TM)</p>
+
+          <p className={styles.paragraphBreak}>
+            HEXIUM-5 CPU at 128MHz
+            <br />
+            Memory Test :&emsp;&emsp; <span className="memory">{memory}</span>
+          </p>
+
+          <p className={styles.paragraphBreak}>
+            Trophy Plug and Play BIOS Extension v1.0A
+            <br />
+            Copyright (C) 1997, Trophy Software, Inc.
+          </p>
+
+          <div className={styles.deviceCheck}>
+            {devices.map((text, index) => (
+              <p key={index} className={styles.deviceCheck}>{text}</p>
+            ))}
           </div>
-          <div className={styles.paragraph}>
-            <h3>(55XWUQOE) Recon r430VX PCIset(TM)</h3>
-          </div>
-          <div className={styles.paragraph}>
-            <h3>HEXIUM-5 CPU at 128MHz</h3>
-            <h3>Memory Test :&emsp;&emsp;32768K OK</h3>
-          </div>
-          <div className={styles.paragraph}>
-            <h3>Trophy Plug and Play BIOS Extension v1.0A</h3>
-            <h3>Copyright (C) 1997, Trophy Software, Inc.</h3>
-            <div className={`${styles.indent + styles.table}`}>
-              <div className={styles.tableRow}>
-                <h3>Detecting IDE Primary Thing</h3>
-                <h3>...</h3>
-                <h3>PCemHD</h3>
-              </div>
-              <div className={styles.tableRow}>
-                <h3 className={styles.col1}>Detecting IDE Secondary Thing</h3>
-                <h3>...</h3>
-                <h3>None</h3>
-              </div>
-              <div className={styles.tableRow}>
-                <h3>Detecting Another Thing</h3>
-                <h3>...</h3>
-                <h3>PCemHD</h3>
-              </div>
-              <div className={styles.tableRow}>
-                <h3>Detecting IDE Blah</h3>
-                <h3>...</h3>
-                <h3>[Press <span style={{"color": "white"}}>F4</span> to skip]_</h3>
-              </div>
-                
-            </div>
-          </div>
+
         </div>
-        <div>
-          <h3>Press <span style={{"color": "white"}}>DEL</span> to enter SETUP</h3>
-          <h3>{date}-r430VX, UMC8469-5A37GH1BC-00</h3>
+        <div className={styles.footer}>
+          <p>Press <span style={{"color": "white"}}>DEL</span> to enter SETUP</p>
+          <p>{date}-r430VX, UMC8469-5A37GH1BC-00</p>
         </div>
-      <img src={luminosityLogo} className={styles.energyLogo} alt="energy-star-logo"/>
       </div>
     </>
   );
